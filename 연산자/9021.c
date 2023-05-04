e435854a4b60775153ab84c7b4f1a725470f058d
MinJun Park <gusoong0829@naver.com>
2023-05-04 14:14:49 +0900

    Jungol_C

--- 연산자/9021.c
+++ 연산자/9021.c
@@ -2,18 +2,23 @@
 // 정수형 변수 a와 b를 만들고 각각 10으로 초기화한 후 다음과 같이 출력하는 프로그램을 작성하시오.
 
 #include <stdio.h>
-#pragma warning(disable:4996)
+#pragma warning(disable : 4996)
 
 int main() {
-    int a = 10, b = 10;
+    int a = 10;
+    int b = 10;
 
-    // 최초값 a = 10, b = 10
+    printf("최초값 a = %d, b = %d\n\n", a, b);
 
-    // a++ = 10, ++b = 11
-    // 실행후 a = 11, b = 11
+    ++b;
+    printf("a++ = %d, ++b = %d\n", a, b);
+    a++;
+    printf("실행후 a = %d, b = %d\n\n", a, b);
 
-    // a-- = 11, --b = 10
-    // 실행후 a = 10, b = 10
+    --b;
+    printf("a-- = %d, --b = %d\n", a, b);
+    a--;
+    printf("실행후 a = %d, b = %d\n", a, b);
 
     return 0;
 }
